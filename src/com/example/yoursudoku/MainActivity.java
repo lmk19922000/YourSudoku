f444c0e86955c72833faebc40f01ba7eedec94a6
truongduy134 <truongduy134@yahoo.com>
2013-12-25 16:22:34 +0800

    Test project setup

--- src/com/example/yoursudoku/MainActivity.java
+++ src/com/example/yoursudoku/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.yoursudoku;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,18 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Button btn1 = (Button) findViewById(R.id.button1);
+		btn1.setOnClickListener(new Button.OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				TextView txtView1 = (TextView) findViewById(R.id.textView1);
+				txtView1.setText("Clicked");
+			}
+			
+		});
 	}
 
 	@Override
